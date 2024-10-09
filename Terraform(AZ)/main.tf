41d33eb66d2607b96733c1a452730fcb9868e244
John Doe <prodyut@kloudgurdian.com>
2024-10-09 17:33:41 +0530

    adding

---
+++ Terraform(AZ)/main.tf
@@ -0,0 +1,18 @@
+module "RG" {
+  source = "./Module/First_Resource_Group/"
+  resource_name = "${var.resource_name}"
+  location_name = "${var.location_name}"
+}
+
+
+module "Redhat" {
+  source = "./Module/Redhat_vm/"
+  redhat_resource_name = "${var.redhat_resource_name}"
+  location_name = "${var.location_name}"
+}
+
+module "Start_stop" {
+  source = "./Module/VM_AutoStrat_Stop"
+  redhat_resource_name = "${var.redhat_resource_name}"
+  location_name = "${var.location_name}"
+}
---
+++ Terraform(AZ)/variable.tf
@@ -0,0 +1,12 @@
+variable "resource_name" {
+  type = string
+}
+
+variable "location_name" {
+  type = string
+}
+
+
+variable "redhat_resource_name" {
+  type = string
+}
