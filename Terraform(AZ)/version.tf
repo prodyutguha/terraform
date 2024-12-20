23d3d2202ee6ed9136386fe2828757f570b2e700
John Doe <prodyut@kloudgurdian.com>
2024-12-20 14:07:10 +0530

    added new login id

--- Terraform(AZ)/version.tf
+++ Terraform(AZ)/version.tf
@@ -2,11 +2,21 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "3.52.0"
+      version = ">= 3.87.0"
+    }
+
+    tls = {
+      source = "hashicorp/tls"
+      version = "~> 4.0.4"
     }
   }
 }
 
 provider "azurerm" {
-    features {}
+  features {}
+
+  client_id       = "5cfd5092-8ede-4841-b3fb-56245200ab93"
+  client_secret   = "bj08Q~DX7s3hhKFpozKJ-ACVWpwikVIfTD5cMbTF"
+  tenant_id       = "e71f06b4-ce8c-42e8-a7e4-582dce3c3f57"
+  subscription_id = "eb245505-9f45-4074-9d46-7e88e7159837"
 }
