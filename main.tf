0368673d5c63e56ba3be29e3feb60913fea83909
prodyut <prodyut@kloudgurdian.com>
2025-07-23 16:55:27 +0530

    updated

--- main.tf
+++ main.tf
@@ -1,23 +1,3 @@
-# module "RG" {
-#   source = "./Module/First_Resource_Group/"
-#   resource_name = "${var.resource_name}"
-#   location_name = "${var.location_name}"
-# }
-
-
-# module "Redhat" {
-#   source = "./Module/Redhat_vm/"
-#   redhat_resource_name = "${var.redhat_resource_name}"
-#   location_name = "${var.location_name}"
-# }
-
-# # module "Start_stop" {
-# #   source = "./Module/VM_AutoStrat_Stop"
-# #   redhat_resource_name = "${var.redhat_resource_name}"
-# #   location_name = "${var.location_name}"
-# # }
-
-# ############## Creating VM using Service Now #############################
 
 resource "azurerm_resource_group" "RG" {
   name     = "${var.resource_group}"
