ca4289669c0ee8e51e172d70ea7ae80e15935236
prodyut <prodyut@kloudgurdian.com>
2025-07-23 19:48:07 +0530

    tfvar update

--- main.tf
+++ main.tf
@@ -1,34 +1,34 @@
-# resource "azurerm_resource_group" "Patching_RG" {
-#   name     = "Patch_Windows"
-#   location = "West Europe"
-# }
+resource "azurerm_resource_group" "Patching_RG" {
+  name     = "Patching_Windows"
+  location = "West Europe"
+}
 
-# resource "azurerm_maintenance_configuration" "maintenance_configuration" {
-#   for_each                 = { for Patch_Group_ID, properties in var.Patch_Group_ID : Patch_Group_ID => properties }
-#   name                     = each.key
-#   resource_group_name      = azurerm_resource_group.Patching_RG.name
-#   location                 = "West Europe"
-#   scope                    = "InGuestPatch"
-#   in_guest_user_patch_mode = "User"
+resource "azurerm_maintenance_configuration" "maintenance_configuration" {
+  for_each                 = { for Patch_Group_ID, properties in var.Patch_Group_ID : Patch_Group_ID => properties }
+  name                     = each.key
+  resource_group_name      = azurerm_resource_group.Patching_RG.name
+  location                 = "West Europe"
+  scope                    = "InGuestPatch"
+  in_guest_user_patch_mode = "User"
 
-#   window {
-#     start_date_time = var.start_date_time
-#     #expiration_date_time  = var.expiration_date_time
-#     duration    = "02:00"
-#     time_zone   = "India Standard Time"
-#     recur_every = each.value.recur_every
-#   }
+  window {
+    start_date_time = var.start_date_time
+    #expiration_date_time  = var.expiration_date_time
+    duration    = "02:00"
+    time_zone   = "India Standard Time"
+    recur_every = each.value.recur_every
+  }
 
-#   install_patches {
-#     windows {
-#       classifications_to_include = var.classifications_to_include
-#       kb_numbers_to_exclude      = var.kb_number_to_exclude
-#       kb_numbers_to_include      = var.kb_number_to_include
-#     }
-#     reboot = "IfRequired"
-#   }
-#   #tags = var.tags
-# }
+  install_patches {
+    windows {
+      classifications_to_include = var.classifications_to_include
+      kb_numbers_to_exclude      = var.kb_number_to_exclude
+      kb_numbers_to_include      = var.kb_number_to_include
+    }
+    reboot = "IfRequired"
+  }
+  #tags = var.tags
+}
 
 
 resource "azurerm_maintenance_assignment_dynamic_scope" "maintenance_assignment_dynamic" {
--- variables.tfvars
+++ variables.tfvars
@@ -1,13 +1,13 @@
 Patch_Group_ID = {
-  "T02-NONPROD-WEU-GR2" = {
+  "T02-NONPROD-WEU-GR0" = {
     start_date_time = "2025-07-23 18:00"
     #expiration_date_time    = "2025-01-19 20:00"
-    recur_every = "Month Third Tuesday"
-  }
-  "T02-NONPROD-WEU-GR3" = {
+    recur_every = "Month Fourth Thursday"
+  },
+  "T02-NONPROD-WEU-GR1" = {
     start_date_time = "2025-07-23 18:00"
     #expiration_date_time    = "2025-01-19 20:00"
-    recur_every = "Month Third Tuesday"
+    recur_every = "Month Fourth Saturday"
   }
 }
 
