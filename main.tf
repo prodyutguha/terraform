2e05f36c2d0cbd06bef85e25756088011e3bcf2e
prodyut <prodyut@kloudgurdian.com>
2025-07-23 20:27:02 +0530

    duration

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "azurerm_maintenance_configuration" "maintenance_configuration" {
   window {
     start_date_time = var.start_date_time
     #expiration_date_time  = var.expiration_date_time
-    duration    = "11:00"
+    duration    = "02:00"
     time_zone   = "India Standard Time"
     recur_every = each.value.recur_every
   }
