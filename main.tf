fed0bc606d341d5bb82a300c69d4051d0d4457f9
prodyut <prodyut@kloudgurdian.com>
2025-08-25 13:00:13 +0530

    update main

--- main.tf
+++ main.tf
@@ -1,10 +1,12 @@
+# Resource Group
 resource "azurerm_resource_group" "Patching_RG" {
   name     = "Patching_Windows"
   location = "West Europe"
 }
 
+# Maintenance Configurations per Patch Group
 resource "azurerm_maintenance_configuration" "maintenance_configuration" {
-  for_each                 = { for Patch_Group_ID, properties in var.Patch_Group_ID : Patch_Group_ID => properties }
+  for_each                 = var.Patch_Group_ID
   name                     = each.key
   resource_group_name      = azurerm_resource_group.Patching_RG.name
   location                 = "West Europe"
@@ -12,40 +14,46 @@ resource "azurerm_maintenance_configuration" "maintenance_configuration" {
   in_guest_user_patch_mode = "User"
 
   window {
-    start_date_time = var.start_date_time
-    #expiration_date_time  = var.expiration_date_time
-    duration    = "02:00"
-    time_zone   = "India Standard Time"
-    recur_every = each.value.recur_every
+    start_date_time = each.value.start_date_time
+    duration        = "02:00"
+    time_zone       = "India Standard Time"
+    recur_every     = each.value.recur_every
   }
 
   install_patches {
     windows {
       classifications_to_include = var.classifications_to_include
-      kb_numbers_to_exclude      = var.kb_number_to_exclude
-      kb_numbers_to_include      = var.kb_number_to_include
+      kb_numbers_to_exclude       = var.kb_number_to_exclude
+      kb_numbers_to_include       = var.kb_numbers_to_include
     }
     reboot = "IfRequired"
   }
-  # tags = var.tags
 }
 
+# Data source: check if dynamic scope already exists
+data "azurerm_maintenance_assignment_dynamic_scope" "existing" {
+  for_each = var.Patch_Group_ID
+  name     = "scope-${each.key}"
+}
 
+# Conditional creation of dynamic scope
 resource "azurerm_maintenance_assignment_dynamic_scope" "maintenance_assignment_dynamic" {
-  for_each                     = { for Patch_Group_ID, properties in var.Patch_Group_ID : Patch_Group_ID => properties }
+  for_each = {
+    for k, v in var.Patch_Group_ID :
+    k => v
+    if try(data.azurerm_maintenance_assignment_dynamic_scope.existing[k].id, null) == null
+  }
+
   name                         = "scope-${each.key}"
   maintenance_configuration_id = azurerm_maintenance_configuration.maintenance_configuration[each.key].id
 
   filter {
-    #locations              = ["East US"]
-    os_types                = ["Windows"]
-    #resource_groups        = ["azurerm_resource_group.RG.name"]
-    resource_types          = ["Microsoft.Compute/virtualMachines"]
-    tag_filter              = "Any"
+    os_types       = ["Windows"]
+    resource_types = ["Microsoft.Compute/virtualMachines"]
+    tag_filter     = "Any"
     tags {
       tag    = "Patch Group ID"
       values = [each.key]
     }
   }
-  depends_on = [ azurerm_maintenance_configuration.maintenance_configuration ]
 }
