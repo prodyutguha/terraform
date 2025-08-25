322f8b2715641ad83aa44fbcc7434cc24d60fe6a
prodyut <prodyut@kloudgurdian.com>
2025-08-25 13:20:03 +0530

    updated format

--- main.tf
+++ main.tf
@@ -38,10 +38,10 @@ resource "azurerm_maintenance_assignment_dynamic_scope" "maintenance_assignment_
 
   filter {
     #locations              = ["East US"]
-    os_types = ["Windows"]
-    resource_groups        = []
-    resource_types = ["Microsoft.Compute/virtualMachines"]
-    tag_filter     = "Any"
+    os_types        = ["Windows"]
+    resource_groups = []
+    resource_types  = ["Microsoft.Compute/virtualMachines"]
+    tag_filter      = "Any"
     tags {
       tag    = "Patch Group ID"
       values = [each.key]
